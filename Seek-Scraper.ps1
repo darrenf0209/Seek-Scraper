4d471320eca411dc7a505418e8631d0b7662aa4b
Darren Flaks <darrenf0209@gmail.com>
2021-05-02 18:18:27 +1000

    Remove cookie notification and parse 'p' fields

--- Seek-Scraper.ps1
+++ Seek-Scraper.ps1
@@ -2,3 +2,13 @@ $baseUri = "https://www.seek.com.au"
 $varUri = "/devops-engineer-jobs/in-All-Melbourne-VIC"
 $fullUri = $baseUri + $varUri
 Write-Host $fullUri
+
+
+# -UseBasicParsing hides the cookies prompt but cannot use the ParsedHtml property
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 
+$r = Invoke-WebRequest -Uri $fullUri -UseBasicParsing
+
+# Writing raw content into 
+$HTML = New-Object -ComObject "HTMLFile"
+$HTML.IHTMLDocument2_write($r.RawContent)
+$HTML.all.tags("p") | % innerText
